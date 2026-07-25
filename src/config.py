3a9461363b1bae657a985de2bbd8d00e7c8ac8bf
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-07-25 21:30:09 +0700

    fix: root path bug

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-ROOT = Path().resolve().parent
+ROOT = Path(__file__).resolve().parent.parent
 DATA = ROOT / 'data'
 RAW_DATA = DATA / 'raw'
 PROCESSED_DATA = DATA / 'processed'
