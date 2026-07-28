e6e11b329ec6bd846fcea0ab325c40c90c35d748
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-07-28 09:45:04 +0700

    feat: add some metrics for benchmark
    - num_params: the number of parameter of each model
    - training_time_minutes: cost of training phase (in minutes)
    - inference_time_cpu_ms: time to process a batch with batch_size = 1 (in ms)
    - per_class_accuracy: accuracy of each class in dataset

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,8 @@
 import json
-import torch
 import yaml
+import time
 import argparse
+import torch
 import torchvision
 from torch.utils.data import DataLoader
 from config import ROOT, PARAMS, TEST_DATA
@@ -27,6 +28,7 @@ def main():
     output_dir = ROOT / "experiments" / args.model
     metrics_path = output_dir / "metrics.json"
     model_path = output_dir / "model.pth"
+    train_stats = output_dir / "train_stats.json"
 
     model_class = MODEL_REGISTRY[args.model]
     model_params = params['models'][args.model]
@@ -37,6 +39,12 @@ def main():
     criterion = torch.nn.CrossEntropyLoss()
     test_loss, correct, total = 0.0, 0, 0
 
+    classes = [
+        "airplane", "automobile", "bird", "cat", "deer",
+        "dog", "frog", "horse", "ship", "truck"
+    ]
+    class_correct = [0] * 10
+    class_total = [0] * 10
     with torch.no_grad():
         for inputs, labels in test_loader:
             inputs, labels = inputs.to(device), labels.to(device)
@@ -51,16 +59,51 @@ def main():
 
             correct += predicted.eq(labels).sum().item()
 
+            # per-class accuracy
+            for label, pred in zip(labels, predicted):
+                class_total[label.item()] += 1
+
+                if label == pred:
+                    class_correct[label.item()] += 1
+
     final_loss = test_loss / len(test_loader)
     final_acc = correct / total
 
-    metrics = {
-        "loss": final_loss,
-        "accuracy": final_acc
+    per_class_accuracy = {
+        classes[i] : round(class_correct[i] / class_total[i], 2) 
+        for i in range(10)
     }
 
-    with open(metrics_path, "w") as f:
-        json.dump(metrics, f, indent=4)
+    dummy_batch = torch.randn(1, 3, 32, 32)
+    model_cpu = model_class(**model_params)
+    # Warm up
+    with torch.no_grad():
+        for _ in range(10):
+            model_cpu(dummy_batch)
+
+    times = []
+    with torch.no_grad():
+        for _ in range(100):
+            start = time.perf_counter()
+            model_cpu(dummy_batch)
+            times.append(time.perf_counter() - start)
+
+    inference_time_ms = (sum(times) / len(times)) * 1000        
+
+    metrics = {
+        "loss"                 : final_loss,
+        "accuracy"             : final_acc,
+        "inference_time_cpu_ms": inference_time_ms,
+        "per_class_accuracy"   : per_class_accuracy
+    }
+
+    with open(train_stats, "r", encoding="utf-8") as f:
+        stats = json.load(f)
+
+    stats.update(metrics)
+
+    with open(metrics_path, "w", encoding="utf-8") as f:
+        json.dump(stats, f, indent=4)
 
     print(f"-> Evaluation Completed. Acc: {final_acc:.4f}, Loss: {final_loss:.4f}")
 
